04b5a9a004430f3a3aa0789310a3a7c3b18bcb08
juan pablo <jpgq29@gmail.com>
2022-10-30 18:10:37 -0500

    refactor: Actor, DTO Actor request add JsonAlias

--- infrastructure/driven-adapters/jpa-repository/src/main/java/co/com/bancolombia/jpa/actor/dto/ActorRequest.java
+++ infrastructure/driven-adapters/jpa-repository/src/main/java/co/com/bancolombia/jpa/actor/dto/ActorRequest.java
@@ -1,5 +1,6 @@
 package co.com.bancolombia.jpa.actor.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
@@ -7,7 +8,8 @@ import javax.validation.constraints.NotNull;
 
 @Data
 public class ActorRequest {
-    @NotNull(message = "actor_id no puede ser vacio")
+    @JsonAlias("Id")
+    @NotNull(message = "Id no puede ser vacio")
     private Long actorId;
     @NotBlank(message = "first_name no puede ser vacio")
     private String firstName;
