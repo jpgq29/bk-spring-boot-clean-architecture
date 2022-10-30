b268bb8b009c564f950a762ede2a1956bc58789b
juan pablo <jpgq29@gmail.com>
2022-10-29 20:58:37 -0500

    refactor: actor: se agregan atributos adicionales

--- domain/model/src/main/java/co/com/bancolombia/model/actor/Actor.java
+++ domain/model/src/main/java/co/com/bancolombia/model/actor/Actor.java
@@ -4,10 +4,15 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @Data
 @Builder(toBuilder = true)
 @AllArgsConstructor
 @NoArgsConstructor
 public class Actor {
     private Long actorId;
+    private String firstName;
+    private String lastName;
+    private Date lastUpdate;
 }
--- infrastructure/driven-adapters/jpa-repository/src/main/java/co/com/bancolombia/jpa/entities/ActorEntity.java
+++ infrastructure/driven-adapters/jpa-repository/src/main/java/co/com/bancolombia/jpa/entities/ActorEntity.java
@@ -7,6 +7,7 @@ import javax.persistence.Table;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Data
 @NoArgsConstructor
@@ -19,4 +20,13 @@ public class ActorEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "actor_id")
     private Long actorId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "last_update")
+    private Date lastUpdate;
 }
