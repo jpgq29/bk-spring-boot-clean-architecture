b333e3fbf2b3b504a9b076d5f22dabe59ae61694
juan pablo <jpgq29@gmail.com>
2022-10-30 17:27:50 -0500

    refactor: Actor, DTO clases nombres y mensajes de error

--- infrastructure/driven-adapters/jpa-repository/src/main/java/co/com/bancolombia/jpa/actor/dto/ActorRequest.java
+++ infrastructure/driven-adapters/jpa-repository/src/main/java/co/com/bancolombia/jpa/actor/dto/ActorRequest.java
@@ -2,9 +2,15 @@ package co.com.bancolombia.jpa.actor.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class ActorRequest {
+    @NotNull(message = "actor_id no puede ser vacio")
     private Long actorId;
+    @NotBlank(message = "first_name no puede ser vacio")
     private String firstName;
+    @NotBlank(message = "last_name no puede ser vacio")
     private String lastName;
 }
--- infrastructure/driven-adapters/jpa-repository/src/main/java/co/com/bancolombia/jpa/actor/dto/ActorResponse.java
+++ infrastructure/driven-adapters/jpa-repository/src/main/java/co/com/bancolombia/jpa/actor/dto/ActorResponse.java
@@ -1,13 +1,20 @@
 package co.com.bancolombia.jpa.actor.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
 public class ActorResponse {
+    @JsonProperty("actor_id")
     private Long actorId;
+    @JsonProperty("first_name")
     private String firstName;
+    @JsonProperty("last_name")
     private String lastName;
+    @JsonProperty("last_update")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private Date lastUpdate;
 }
