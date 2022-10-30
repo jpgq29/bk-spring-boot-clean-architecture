17731f8fd9c2234e36ec5298b8e67b4f236677a8
juan pablo <jpgq29@gmail.com>
2022-10-29 21:01:04 -0500

    feac: Iconversion e implementacion para actor

---
+++ infrastructure/driven-adapters/jpa-repository/src/main/java/co/com/bancolombia/jpa/actor/dto/ActorConversionImp.java
@@ -0,0 +1,20 @@
+package co.com.bancolombia.jpa.actor.dto;
+
+import co.com.bancolombia.jpa.common.IConversion;
+import co.com.bancolombia.model.actor.Actor;
+import org.reactivecommons.utils.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ActorConversionImp implements IConversion<Actor, ActorResponse> {
+    @Autowired
+    private ObjectMapper mapper;
+    @Override
+    public List<ActorResponse> listConversion(List<Actor> actor) {
+        return actor.stream().map(dato -> mapper.map(dato,ActorResponse.class)).collect(Collectors.toList());
+    }
+}
---
+++ infrastructure/driven-adapters/jpa-repository/src/main/java/co/com/bancolombia/jpa/common/IConversion.java
@@ -0,0 +1,7 @@
+package co.com.bancolombia.jpa.common;
+
+import java.util.List;
+
+public interface IConversion<E, D> {
+    List<D> listConversion(List<E> data);
+}
